5c7388b54bd0bf3e6a978632c259dab85465a67c
Amy Zhou <zhouamy77@gmail.com>
2018-08-06 19:16:27 +0000

    header and navbar. navbar links don't work

--- application.py
+++ application.py
@@ -8,3 +8,23 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/events")
+def events():
+    return render_template("events.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
