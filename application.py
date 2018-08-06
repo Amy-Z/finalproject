8686c1f99e234235a34b0e4a720c2a8ee20d8014
Amy Zhou <zhouamy77@gmail.com>
2018-08-06 00:39:34 +0000

    initial commit

---
+++ application.py
@@ -0,0 +1,10 @@
+import os
+import requests
+import datetime
+
+from flask import Flask, render_template, jsonify, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
