129cf50e3aa05cafe75e229727a47a94868967e9
Amy Zhou <zhouamy77@gmail.com>
2018-08-07 21:13:18 +0000

    moved html files into templates. trying out themewagon

--- application.py
+++ application.py
@@ -9,22 +9,31 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+
+# list of all channels
+channel_list = ['general']
+# store messages here
+messages = []
+allchann = ['']
+# allchann = {'general': None}
+
+
+# single page application at index.html
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/events")
-def events():
-    return render_template("events.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+# with socket, append the sent messages to the list and store messages server side
+@socketio.on("submit message")
+def chats(data):
+    allchann.append(data["text"])
+    datetime.datetime
+    emit("receive", data, broadcast=True)
 
-@app.route("/register")
-def register():
-    return render_template("register.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+# with socket, append the channel to the list of created channels
+@socketio.on("existchann")
+def exist(chann):
+    channel_list.append(chann["group"])
+    emit("here", chann, broadcast=True)
